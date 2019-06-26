71352c43c95ae0e3cc33f28683b3cbfc90a985a0
Isaac Philippe <52184718+isaac2222@users.noreply.github.com>
2019-06-25 23:34:11 -0300

    NewCommit

---
+++ ClassesParametros/ClassesParametros/Teste.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesParametros
+{
+    class Teste
+    {
+        public void Soma(ref int valor)
+        {
+            valor += 5;
+        }
+        public void Subtracao(ref int valor)
+        {
+            valor -= 5;
+        }
+    }
+}
