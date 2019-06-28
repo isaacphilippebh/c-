728c2a2deb36db30e2ceb9907c9191f3ef39d647
Isaac Philippe <52184718+isaac2222@users.noreply.github.com>
2019-06-27 21:31:04 -0300

    Enumeradores

---
+++ Enumeradores/Enumeradores/DiasDaSemana.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumeradores
+{
+    class DiasDaSemana
+    {
+        public enum Dia
+        {
+            Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado
+        }
+        public void ApresentarDia(Dia diaSemana)
+        {
+            Console.WriteLine(diaSemana);
+        }
+    }
+}
--- Enumeradores/Enumeradores/Form1.cs
+++ Enumeradores/Enumeradores/Form1.cs
@@ -16,5 +16,11 @@ namespace Enumeradores
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DiasDaSemana dd = new DiasDaSemana();
+            dd.ApresentarDia(DiasDaSemana.Dia.Domingo);
+        }
     }
 }
